33aa9f575c12ba57304b04a53065513cab114026
aaron moroney <aareon.createdesign@gmail.com>
2023-02-07 12:25:20 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './styles/utils/_variables.scss'
 function App() {
   return (
     <div className="App">
-      <h6>2021</h6>
-      <h1>SKULLCANDY COLLECTION</h1>
-      <h2>Crusher ANC personalized</h2>
-      <h6>View all</h6>
-      <p> here is some long winded information about the headphones</p>
+      <p>hello world</p>
     </div>
   );
 }
